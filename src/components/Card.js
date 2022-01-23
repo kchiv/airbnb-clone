e68d24ddd0625dd3dd83829df3b2a89d66393507
Kyle Chivers <chiverskyle@gmail.com>
2022-01-23 08:09:46 -0800

    Created and styled static card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,17 @@ import React from "react"
 
 function Card() {
     return (
-        <section className="card"></section>
+        <div className="card">
+            <img src="../images/katie-zaferes.png" className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
     );
 }
 
