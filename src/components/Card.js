2117939cff465e4497c49de747ad692ed84d54ad
Kyle Chivers <chiverskyle@gmail.com>
2022-01-23 13:39:35 -0800

    Fixed naming of location prop

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ function Card(props) {
                 <img src="../images/star.png" className="card--star" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
