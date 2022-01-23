5834827f38f9161e67a9c3df439526a424ca9b20
Kyle Chivers <chiverskyle@gmail.com>
2022-01-23 14:45:11 -0800

    Conditional to set different badge values
    Developed conditional logic that will set different badgeText values depending on a few different conditions.
    
    Also, updating the JSX condition from checking if openSpots prop is 0 to checking if badgeText simply has any value. If badgeText does not have any value or in other words, it's undefined, we won't render any badge. If badgeText does have any value, we'll render the badge.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import React from "react"
 
 function Card(props) {
-    console.log(props)
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
