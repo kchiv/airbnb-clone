e5011150c4ef67aa086c8b653893063a90bd1ca3
Kyle Chivers <chiverskyle@gmail.com>
2022-01-23 14:34:44 -0800

    Conditional render Sold Out badge
    Utilized conditional rendering in JSX to render Sold Out badge. First we have the condition that checks if openSpots evaluates to 0 and if it does, then we render out the subsequent JSX element.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ function Card(props) {
     console.log(props)
     return (
         <div className="card">
+            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
             <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
