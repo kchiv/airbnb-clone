a28e371c1bc388e81979abc11086ee9cfc3cf874
Kyle Chivers <chiverskyle@gmail.com>
2022-01-23 07:42:36 -0800

    Add basic Card component shell to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Card() {
+    return (
+        <section className="card"></section>
+    );
+}
+
+export default Card;
