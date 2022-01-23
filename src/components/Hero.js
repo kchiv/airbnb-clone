745961d67e4fb47ae48b9cfbad5c677764b893af
Kyle Chivers <chiverskyle@gmail.com>
2022-01-23 07:33:44 -0800

    added content to hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,12 @@ import React from "react"
 
 function Hero() {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <img src="../images/photo-grid.png" />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
     );
 }
 
